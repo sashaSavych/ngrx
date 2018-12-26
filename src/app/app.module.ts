3242e1ed4b4bec039cf237e044f5b4d9b4c97ad2
Oleksandr Savych <oleksandr.i.savych@thomascookonline.com>
2018-12-26 11:23:59 +0200

    add StoreRouterConnectingModule, StoreDevtoolsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { environment } from '../environments/environment';
 
 import { CarsEffects } from './redux/cars.effects';
 import { StoreModule } from '@ngrx/store';
 import { carsReducer } from './redux/cars.reducer';
 import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppComponent } from './app.component';
 import { CarComponent } from './components/car/car.component';
@@ -21,6 +25,11 @@ import { CarsService } from './app.service';
     EffectsModule.forRoot([CarsEffects]),
     StoreModule.forRoot({carPage: carsReducer}),
     HttpClientModule,
+    RouterModule.forRoot([
+      {path: '', component: AppComponent}
+    ]),
+    StoreRouterConnectingModule,
+    environment.production ? [] : StoreDevtoolsModule.instrument()
   ],
   providers: [CarsService],
   bootstrap: [AppComponent]
